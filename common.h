b02719a27bde766507c1aa678f3d46804f37aaf7
root <marshuttle@gmai.com>
2010-09-30 14:03:11 +0800

    测试版：升级main.cpp 2.y

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef _LLVM_COMMON_H
+#define _LLVM_COMMON_H
+
+#include "llvm/DerivedTypes.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/Support/IRBuilder.h"
+
+using namespace llvm;
+#endif
