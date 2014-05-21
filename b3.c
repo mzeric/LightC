b73daa6c51c85b30a9c0e72c1c84530a78cf6e6f
tianxiao <tianxiao.xtx@gmail.com>
2014-05-22 02:25:05 +0800

    frame up, ready 4 debug

---
+++ b3.c
@@ -0,0 +1,19 @@
+int  foo, fool;
+int hello;
+int dump(void);
+int func(void){
+
+	int d;
+	if(d > 2){
+		int f1;
+		d = 3;
+	}else{
+
+		int f2;
+	}
+
+	int f;
+}
+int tail(int t){
+	//return 3;
+}
